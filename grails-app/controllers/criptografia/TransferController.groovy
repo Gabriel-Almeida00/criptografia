92bd65987d0d5526f7740afa2050ab376d9b7726
Gabriel <gaamdal557@gmail.com>
2024-02-10 21:09:37 -0300

    feat: método para criar

--- grails-app/controllers/criptografia/TransferController.groovy
+++ grails-app/controllers/criptografia/TransferController.groovy
@@ -1,83 +1,18 @@
 package criptografia
 
-import grails.validation.ValidationException
-import static org.springframework.http.HttpStatus.CREATED
-import static org.springframework.http.HttpStatus.NOT_FOUND
-import static org.springframework.http.HttpStatus.NO_CONTENT
-import static org.springframework.http.HttpStatus.OK
-import static org.springframework.http.HttpStatus.UNPROCESSABLE_ENTITY
-
-import grails.gorm.transactions.ReadOnly
-import grails.gorm.transactions.Transactional
-
 class TransferController {
 
     TransferService transferService
 
     static responseFormats = ['json', 'xml']
-    static allowedMethods = [list: "GET", save: "POST", update: "PUT", delete: "DELETE"]
+    static allowedMethods = [list: "GET", create: "POST"]
 
     List<Transfer> list() {
         respond(transferService.list()) as List<Transfer>
     }
 
-    def show(Long id) {
-        respond transferService.get(id)
-    }
-
-    @Transactional
-    def save(Transfer transfer) {
-        if (transfer == null) {
-            render status: NOT_FOUND
-            return
-        }
-        if (transfer.hasErrors()) {
-            transactionStatus.setRollbackOnly()
-            respond transfer.errors
-            return
-        }
-
-        try {
-            transferService.save(transfer)
-        } catch (ValidationException e) {
-            respond transfer.errors
-            return
-        }
-
-        respond transfer, [status: CREATED, view: "show"]
-    }
-
-    @Transactional
-    def update(Transfer transfer) {
-        if (transfer == null) {
-            render status: NOT_FOUND
-            return
-        }
-        if (transfer.hasErrors()) {
-            transactionStatus.setRollbackOnly()
-            respond transfer.errors
-            return
-        }
-
-        try {
-            transferService.save(transfer)
-        } catch (ValidationException e) {
-            respond transfer.errors
-            return
-        }
-
-        respond transfer, [status: OK, view: "show"]
-    }
-
-    @Transactional
-    def delete(Long id) {
-        if (id == null) {
-            render status: NOT_FOUND
-            return
-        }
-
-        transferService.delete(id)
-
-        render status: NO_CONTENT
+    def create(Transfer transfer) {
+        transferService.create(transfer)
+        render(contentType: responseFormats, text: 'mensagemDeErro')
     }
 }
--- grails-app/controllers/criptografia/UrlMappings.groovy
+++ grails-app/controllers/criptografia/UrlMappings.groovy
@@ -3,11 +3,12 @@ package criptografia
 class UrlMappings {
 
     static mappings = {
-        group ("/criptografia") {
-            get "/all"(controller: 'transfer', action: 'list')
-        }
 
-        "/"(controller: 'application', action:'index')
+        get "/all" (controller: 'transfer', action: 'list')
+        post "/create" (controller: 'transfer', action: 'create')
+
+
+        "/"(controller: 'application', action: 'index')
         "500"(view: '/error')
         "404"(view: '/notFound')
     }
